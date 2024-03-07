7c412d39f593f9d7fb346dc0fa72c348dc9aa05c
Bukharova Tatyana <tatyana.bu@inbox.ru>
2024-03-07 14:49:36 +0300

    Tasks added

---
+++ Task3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете
+// его элементы, начиная с конца. Использовать
+// рекурсию, не использовать циклы.
+
+void RecursiveArrayPrint (int [] arr, int index = 0)
+{
+    if (index == arr.Length)
+    { 
+    return;
+    }
+    RecursiveArrayPrint(arr, index + 1);
+    Console.Write($"{arr[index]} ");
+  
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (var item in arr)
+    {
+        Console.Write($"{item} ");
+    }    
+    Console.WriteLine();
+}
+
+
+int [] array = {1, 2, 5, 0, 10, 34};
+PrintArray(array);
+RecursiveArrayPrint(array);
